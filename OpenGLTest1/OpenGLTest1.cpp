b23f4eadf70b41b3cc1a4cc47cb5eda2c0ea8348
sak9188 <sak9188@163.com>
2020-03-24 21:25:09 +0800

    modify a wrong code line

--- OpenGLTest1/OpenGLTest1.cpp
+++ OpenGLTest1/OpenGLTest1.cpp
@@ -7,8 +7,6 @@
 #include "BaseClass\Texture.h"
 #include "BaseClass\Camera.h"
 
-#include "include\assimp\config.h"
-
 int init(GLFWwindow* &window);
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
